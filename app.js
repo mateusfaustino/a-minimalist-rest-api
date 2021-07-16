91379c3a2d7b172077342340de867f694dc7c466
Mateus Faustino <mateusfaustino2017@gmail.com>
2021-07-16 15:53:28 -0300

    fixxing port again

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.port || 8080
+const PORT = process.env.port || 8877
 
 app.get('/route-2',(req,res)=>{
     res.json(
